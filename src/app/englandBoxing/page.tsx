79427c1fb1854d60d18dfb7d2439b513ffe66f00
marsad <marsad11223@gmail.com>
2024-10-28 16:02:06 +0500

    responsive section England boxing

---
+++ src/app/englandBoxing/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Hero from "../home/_sections/Hero";
+import EnglandBoxingCardBox from "./_components/EnglandBoxing";
+import Footer from "../_components/Footer";
+
+export default function EnglandBoxing() {
+  return (
+    <>
+      <Hero />
+      <EnglandBoxingCardBox />
+      <Footer />
+    </>
+  );
+}
