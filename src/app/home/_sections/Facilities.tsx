9894279e9b5c7f75010887a9a3d80a2051b945ce
Marsad Ghanvi <marsad11223@gmail.com>
2024-10-31 14:18:46 +0500

    hover effects on facilities section

--- src/app/home/_sections/Facilities.tsx
+++ src/app/home/_sections/Facilities.tsx
@@ -56,12 +56,27 @@ export default function Facilities() {
           {/* grid images */}
           <Grid container spacing={1.25}>
             <Grid item xs={12} sm={4} md={4}>
-              <Box sx={{ height: { sm: "100%" }, width: "auto" }}>
+              <Box
+                sx={{
+                  height: { sm: "100%" },
+                  width: "auto",
+                  overflow: "hidden",
+                  transition: "border 0.3s ease",
+                  "&:hover": {
+                    border: "1.5px solid #ffffff",
+                  },
+                }}
+              >
                 <Box
                   sx={{
                     display: { xs: "block", sm: "none", md: "block" },
                     width: "100%",
                     height: "100%",
+                    overflow: "hidden",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
                 >
                   <Image
@@ -79,6 +94,10 @@ export default function Facilities() {
                     display: { xs: "none", sm: "block", md: "none" },
                     width: "100%",
                     height: "100%",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
                 >
                   <Image
@@ -94,12 +113,26 @@ export default function Facilities() {
               </Box>
             </Grid>
             <Grid item xs={12} sm={8} md={2}>
-              <Box sx={{ height: { sm: "100%" }, width: "auto" }}>
+              <Box
+                sx={{
+                  height: { sm: "100%" },
+                  width: "auto",
+                  overflow: "hidden",
+                  transition: "border 0.3s ease",
+                  "&:hover": {
+                    border: "1.5px solid #ffffff",
+                  },
+                }}
+              >
                 <Box
                   sx={{
                     display: { xs: "block", sm: "none", md: "block" },
                     width: "100%",
                     height: "100%",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
                 >
                   <Image
@@ -117,6 +150,10 @@ export default function Facilities() {
                     display: { xs: "none", sm: "block", md: "none" },
                     width: "100%",
                     height: "100%",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
                 >
                   <Image
@@ -132,102 +169,215 @@ export default function Facilities() {
               </Box>
             </Grid>
             <Grid item xs={12} sm={8} md={4}>
-              <Box sx={{ height: { sm: "100%" }, width: "auto" }}>
-                <Image
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
+              <Box
+                sx={{
+                  height: { sm: "100%" },
+                  overflow: "hidden",
+                  transition: "border 0.3s ease",
+                  "&:hover": {
+                    border: "1.5px solid #ffffff",
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    height: { sm: "100%" },
+                    width: "auto",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
-                  src={webp.F3}
-                  alt="facility3"
-                />
+                >
+                  <Image
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    src={webp.F3}
+                    alt="facility3"
+                  />
+                </Box>
               </Box>
             </Grid>
             <Grid item xs={12} sm={4} md={2}>
-              <Box sx={{ height: { sm: "100%" }, width: "auto" }}>
-                <Image
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
+              <Box
+                sx={{
+                  overflow: "hidden",
+                  transition: "border 0.3s ease",
+                  "&:hover": {
+                    border: "1.5px solid #ffffff",
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    height: { sm: "100%" },
+                    width: "auto",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
-                  src={webp.F4}
-                  alt="facility4"
-                />
+                >
+                  <Image
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    src={webp.F4}
+                    alt="facility4"
+                  />
+                </Box>
               </Box>
             </Grid>
             <Grid item xs={12} sm={4} md={2}>
-              <Box sx={{ height: { sm: "100%" }, width: "auto" }}>
-                <Image
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
+              <Box
+                sx={{
+                  height: { sm: "100%" },
+                  overflow: "hidden",
+                  transition: "border 0.3s ease",
+                  "&:hover": {
+                    border: "1.5px solid #ffffff",
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    height: { sm: "100%" },
+                    width: "auto",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
-                  src={webp.F5}
-                  alt="facility5"
-                />
+                >
+                  <Image
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    src={webp.F5}
+                    alt="facility5"
+                  />
+                </Box>
               </Box>
             </Grid>
             <Grid item xs={12} sm={8} md={4}>
-              <Box sx={{ height: { sm: "100%" }, width: "auto" }}>
-                <Image
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
+              <Box
+                sx={{
+                  height: { sm: "100%" },
+                  overflow: "hidden",
+                  transition: "border 0.3s ease",
+                  "&:hover": {
+                    border: "1.5px solid #ffffff",
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    height: { sm: "100%" },
+                    width: "auto",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
-                  src={webp.F6}
-                  alt="facility6"
-                />
+                >
+                  <Image
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    src={webp.F6}
+                    alt="facility6"
+                  />
+                </Box>
               </Box>
             </Grid>
             <Grid item xs={12} sm={8} md={2}>
               <Box sx={{ height: { sm: "100%" }, width: "auto" }}>
                 <Box
                   sx={{
-                    display: { xs: "block", sm: "none", md: "block" },
-                    width: "100%",
-                    height: "100%",
+                    height: { sm: "100%" },
+                    overflow: "hidden",
+                    transition: "border 0.3s ease",
+                    "&:hover": {
+                      border: "1.5px solid #ffffff",
+                    },
                   }}
                 >
-                  <Image
-                    style={{
+                  <Box
+                    sx={{
+                      display: { xs: "block", sm: "none", md: "block" },
                       width: "100%",
                       height: "100%",
-                      objectFit: "cover",
+                      transition: "transform 0.3s ease",
+                      "&:hover": {
+                        transform: "scale(1.1)",
+                      },
                     }}
-                    src={webp.F7}
-                    alt="facility7"
-                  />
-                </Box>
-                <Box
-                  sx={{
-                    display: { xs: "none", sm: "block", md: "none" },
-                    width: "100%",
-                    height: "100%",
-                  }}
-                >
-                  <Image
-                    style={{
+                  >
+                    <Image
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                      src={webp.F7}
+                      alt="facility7"
+                    />
+                  </Box>
+
+                  <Box
+                    sx={{
+                      display: { xs: "none", sm: "block", md: "none" },
                       width: "100%",
                       height: "100%",
-                      objectFit: "cover",
+                      transition: "transform 0.3s ease",
+                      "&:hover": {
+                        transform: "scale(1.1)",
+                      },
                     }}
-                    src={webp.F8}
-                    alt="facility8"
-                  />
+                  >
+                    <Image
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                      src={webp.F8}
+                      alt="facility8"
+                    />
+                  </Box>
                 </Box>
               </Box>
             </Grid>
             <Grid item xs={12} sm={4} md={4}>
-              <Box sx={{ height: { sm: "100%" }, width: "auto" }}>
+              <Box
+                sx={{
+                  height: { sm: "100%" },
+                  width: "auto",
+                  overflow: "hidden",
+                  transition: "border 0.3s ease",
+                  "&:hover": {
+                    border: "1.5px solid #ffffff",
+                  },
+                }}
+              >
                 <Box
                   sx={{
                     display: { xs: "block", sm: "none", md: "block" },
                     width: "100%",
                     height: "100%",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
                 >
                   <Image
@@ -245,6 +395,10 @@ export default function Facilities() {
                     display: { xs: "none", sm: "block", md: "none" },
                     width: "100%",
                     height: "100%",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
                 >
                   <Image
