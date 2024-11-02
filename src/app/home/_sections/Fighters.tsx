b85739297ae08e7f2c0888893f151880d56a06a3
Marsad Ghanvi <marsad11223@gmail.com>
2024-11-03 02:21:51 +0500

    Add stable version of responsive layout for Fighters section

--- src/app/home/_sections/Fighters.tsx
+++ src/app/home/_sections/Fighters.tsx
@@ -3,16 +3,38 @@ import Image, { StaticImageData } from "next/image";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import webp from "@/app/_assets/webp";
+import svgs from "@/app/_assets/svgs";
 
-// Array of fighter data
 const fighters = [
   {
+    index: 0,
     name: "Allen Rivera",
     description:
       "A fierce middleweight champion known for her precision strikes",
     imageSrc: webp.Fighter2,
   },
+  {
+    index: 1,
+    name: "Another Fighter",
+    description: "A heavyweight champion known for his endurance and strength",
+    imageSrc: webp.Fighter3,
+  },
+  {
+    index: 2,
+    name: "Yet Another Fighter",
+    description: "A heavyweight champion known for his endurance and strength",
+    imageSrc: webp.Fighter3,
+  },
+];
+
+const fighterImages = [
+  { index: 0, imagePath: webp.Fighter2, alt: "fighter2" },
+  { index: 1, imagePath: webp.Fighter2, alt: "fighter2" },
+  { index: 2, imagePath: webp.Fighter3, alt: "fighter3" },
 ];
 
 export default function Fighters() {
@@ -20,6 +42,26 @@ export default function Fighters() {
     AOS.init({ duration: 500, once: true });
     AOS.refresh();
   }, []);
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <CustomRightArrow />,
+    prevArrow: <CustomLeftArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 2 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 1 },
+      },
+    ],
+  };
 
   return (
     <Box
@@ -45,7 +87,6 @@ export default function Fighters() {
           width: "100%",
         }}
       >
-        {/* heading */}
         <Typography
           data-aos="fade-right"
           data-aos-duration="1000"
@@ -70,17 +111,61 @@ export default function Fighters() {
         >
           our schedule
         </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: { xs: "50px", md: "70px", xl: "100px" },
+            flexDirection: { xs: "column-reverse", lg: "row" },
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: { xs: "row", lg: "column" },
+              gap: "25px",
+            }}
+          >
+            {fighterImages.map((img, index) => (
+              <Box
+                key={index}
+                sx={{
+                  width: { xs: "50px", md: "77px", xl: "90px" },
+                  height: { xs: "50px", md: "77px", xl: "90px" },
+                  position: "relative",
+                }}
+              >
+                <Image
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    objectFit: "cover",
+                    position: "absolute",
+                    top: "0",
+                  }}
+                  src={img.imagePath}
+                  alt={img.alt}
+                />
+              </Box>
+            ))}
+          </Box>
 
-        {/* card box */}
-        <Box>
-          {fighters.map((fighter, index) => (
-            <FighterCard
-              key={index}
-              name={fighter.name}
-              description={fighter.description}
-              imageSrc={fighter.imageSrc}
-            />
-          ))}
+          {/* React Slick Slider */}
+          <Box sx={{ maxWidth: { xs: "300px", sm: "1000px" }, width: "100%" }}>
+            <Slider {...sliderSettings}>
+              {fighters.map((fighter, index) => (
+                <FighterCard
+                  key={index}
+                  name={fighter.name}
+                  description={fighter.description}
+                  imageSrc={fighter.imageSrc}
+                />
+              ))}
+            </Slider>
+          </Box>
         </Box>
       </Box>
     </Box>
@@ -96,73 +181,130 @@ type FighterCardProps = {
 export function FighterCard({ name, description, imageSrc }: FighterCardProps) {
   return (
     <Box
-      sx={{
-        width: {
-          xs: "200px",
-          sm: "250px",
-          md: "300px",
-          borderWidth: { xs: "1px", md: "1.7px" },
-          borderStyle: "solid",
-          borderColor: "#ffffff",
-        },
-      }}
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
     >
       <Box
         sx={{
-          maxWidth: "300px",
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <Image
-          style={{
-            height: "100%",
-            width: "100%",
-            objectFit: "cover",
-          }}
-          src={imageSrc}
-          alt={name}
-        />
-      </Box>
-
-      <Box
-        sx={{
-          padding: {
-            xs: "10px",
-            md: "13px",
+          width: {
+            xs: "200px",
+            sm: "250px",
+            md: "300px",
+            borderWidth: { xs: "1px", md: "1.7px" },
+            borderStyle: "solid",
+            borderColor: "#ffffff",
           },
-          display: "flex",
-          flexDirection: "column",
-          gap: "8px",
-          backgroundColor: "#0D0D0DB2",
-          marginTop: "-3px",
-          borderTopWidth: { xs: "1px", md: "1.7px" },
-          borderTopStyle: "solid",
-          borderTopColor: "#ffffff",
         }}
       >
-        <Typography
+        <Box
           sx={{
-            color: "#FFFFFF",
-            fontWeight: "700",
-            fontFamily: "lato, sans-serif",
-            fontSize: { xs: "14px", md: "16px", lg: "18px" },
+            maxWidth: "300px",
+            width: "100%",
+            height: "100%",
           }}
         >
-          {name}
-        </Typography>
+          <Image
+            style={{
+              height: "100%",
+              width: "100%",
+              objectFit: "cover",
+            }}
+            src={imageSrc}
+            alt={name}
+          />
+        </Box>
 
-        <Typography
+        <Box
           sx={{
-            color: "#ffffff",
-            fontWeight: "400",
-            fontFamily: "lato, sans-serif",
-            fontSize: "12px",
+            padding: {
+              xs: "10px",
+              md: "13px",
+            },
+            display: "flex",
+            flexDirection: "column",
+            gap: "8px",
+            backgroundColor: "#0D0D0DB2",
+            borderTopWidth: { xs: "1px", md: "1.7px" },
+            borderTopStyle: "solid",
+            borderTopColor: "#ffffff",
           }}
         >
-          {description}
-        </Typography>
+          <Typography
+            sx={{
+              color: "#FFFFFF",
+              fontWeight: "700",
+              fontFamily: "lato, sans-serif",
+              fontSize: { xs: "14px", md: "16px", lg: "18px" },
+            }}
+          >
+            {name}
+          </Typography>
+
+          <Typography
+            sx={{
+              color: "#ffffff",
+              fontWeight: "400",
+              fontFamily: "lato, sans-serif",
+              fontSize: "12px",
+            }}
+          >
+            {description}
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const CustomLeftArrow = (props: any) => {
+  const { onClick } = props;
+  return (
+    <>
+      <Box
+        onClick={onClick}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: { xs: "-10px", md: "-36px", xl: "-60px" },
+          transform: "translateY(-50%)  rotate(180deg)",
+          maxWidth: { xs: "25px", md: "30px", lg: "35px", xl: "42px" },
+          width: "100%",
+          zIndex: "1",
+        }}
+      >
+        <Image
+          style={{ height: "100%", width: "100%", objectFit: "contain" }}
+          src={svgs.Arrow}
+          alt="arrow left"
+        />
+      </Box>
+    </>
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const CustomRightArrow = (props: any) => {
+  const { onClick } = props;
+  return (
+    <>
+      <Box
+        onClick={onClick}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          right: { xs: "-10px", md: "-36px", xl: "-60px" },
+          zIndex: "1",
+          transform: "translateY(-50%)",
+          maxWidth: { xs: "25px", md: "30px", lg: "35px", xl: "42px" },
+          width: "100%",
+        }}
+      >
+        <Image
+          style={{ height: "100%", width: "100%", objectFit: "contain" }}
+          src={svgs.Arrow}
+          alt="arrow left"
+        />
+      </Box>
+    </>
+  );
+};
