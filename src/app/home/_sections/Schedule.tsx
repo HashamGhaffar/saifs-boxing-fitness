d1b1e5aba2218e9edc3b648291ebc0de12dbf2f8
Marsad Ghanvi <marsad11223@gmail.com>
2025-01-06 18:44:20 +0500

    mapping table data

--- src/app/home/_sections/Schedule.tsx
+++ src/app/home/_sections/Schedule.tsx
@@ -17,6 +17,7 @@ export default function Schedule() {
     AOS.init({ duration: 500, once: true });
     AOS.refresh();
   }, []);
+
   const headingStyle = {
     border: "none",
     backgroundColor: "#ffffff",
@@ -28,312 +29,204 @@ export default function Schedule() {
     borderRadius: "6px",
     textTransform: "uppercase",
   };
+
   const paragraphStyle = {
     backgroundColor: "#0D0D0DB2",
-    border: "1.6px solid #D9D9D9 ",
+    border: "1.6px solid #D9D9D9",
     textWrap: "nowrap",
     textAlign: "center",
     borderRadius: "6px",
   };
+
   const topic = {
     fontSize: "12px",
     fontWeight: "900",
   };
-  // const description = {
-  //   fontSize: "10px",
-  //   fontWeight: "700",
-  //   color: "#D9D9D9",
-  // };
+
+  const tableRows = [
+    {
+      time: "10:00AM - 11:00AM",
+      activities: ["-", "-", "-", "-", "-", "Girls Only", "-"],
+    },
+    {
+      time: "11:00AM - 12:00AM",
+      activities: [
+        "-",
+        "Afternoon Session",
+        "-",
+        "-",
+        "-",
+        "5-8 Years Old",
+        "5-8 Years Old",
+      ],
+    },
+    {
+      time: "12:00PM - 1:00PM",
+      activities: [
+        "Afternoon Session",
+        "-",
+        "Afternoon Session",
+        "-",
+        "-",
+        "9 Years Old +",
+        "9 Years Old +",
+      ],
+    },
+    {
+      time: "1:00PM - 2:00PM",
+      activities: ["-", "-", "-", "-", "-", "Open Class", "Open Class"],
+    },
+    {
+      time: "5:30PM - 7:00PM",
+      activities: [
+        "Adults & Senior",
+        "Adults & Senior",
+        "Adults & Senior",
+        "Adults & Senior",
+        "Adults & Senior",
+        "-",
+        "-",
+      ],
+    },
+    {
+      time: "7:00PM - 8:00PM",
+      activities: [
+        "Juniors Boxing",
+        "Juniors Boxing",
+        "Juniors Boxing",
+        "Juniors Boxing",
+        "Juniors Boxing",
+        "-",
+        "-",
+      ],
+    },
+    {
+      time: "8:00PM - 9:00PM",
+      activities: [
+        "For All",
+        "For All",
+        "For All",
+        "For All",
+        "For All",
+        "-",
+        "-",
+      ],
+    },
+  ];
 
   return (
-    <>
+    <Box
+      sx={{
+        backgroundImage: "url(bgSchedule.webp)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        padding: {
+          xs: "40px 20px 40px",
+          sm: "60px 50px 60px",
+          md: "60px 50px 60px",
+          lg: "80px 100px 80px",
+        },
+      }}
+    >
       <Box
         sx={{
-          backgroundImage: "url(bgSchedule.webp)",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "100vh",
-          padding: {
-            xs: "40px 20px 40px",
-            sm: "60px 50px 60px",
-            md: "60px 50px 60px",
-            lg: "80px 100px 80px",
-          },
+          maxWidth: "1600px",
+          width: "100%",
         }}
       >
-        <Box
+        {/* Heading */}
+        <Typography
+          data-aos="fade-right"
+          data-aos-duration="1000"
           sx={{
-            maxWidth: "1600px",
-            width: "100%",
+            fontSize: {
+              xs: "24px",
+              sm: "30px",
+              md: "40px",
+              lg: "48px",
+              xl: "64px",
+            },
+            fontWeight: "400",
+            fontFamily: "Anton, sans-serif",
+            color: "#F63333",
+            textTransform: "uppercase",
+            paddingBottom: {
+              xs: "30px",
+              sm: "40px",
+              xl: "80px",
+            },
           }}
         >
-          {/* heading  */}
-          <Typography
-            data-aos="fade-right"
-            data-aos-duration="1000"
-            sx={{
-              fontSize: {
-                xs: "24px",
-                sm: "30px",
-                md: "40px",
-                lg: "48px",
-                xl: "64px",
-              },
-              fontWeight: "400",
-              fontFamily: "Anton, sans-serif",
-              color: "#F63333",
-              textTransform: "uppercase",
-              paddingBottom: {
-                xs: "30px",
-                sm: "40px",
-                xl: "80px",
-              },
-            }}
-          >
-            our schedule
-          </Typography>
-          {/* table first Box */}
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <Box sx={{ maxWidth: "1600px", width: "100%" }}>
-              <TableContainer data-aos="fade-left" data-aos-duration="1000">
-                <Table
-                  sx={{
-                    "& .MuiTableCell-root": { padding: "10px 5px" },
-                    tableLayout: "auto",
-                    borderCollapse: "separate",
-                    borderSpacing: { xs: "20px 15px", xl: "30px 25px" },
-                  }}
-                >
-                  <TableRow>
-                    <TableCell
-                      sx={{
-                        ...headingStyle,
-                        display: "flex",
-                        justifyContent: "center",
-                      }}
-                    >
-                      <Box sx={{ height: "55px", width: "95px" }}>
-                        <Image
-                          style={{
-                            height: "100%",
-                            width: "100%",
-                            objectFit: "contain",
-                          }}
-                          src={svgs.Clock}
-                          alt="clock"
-                        />
-                      </Box>
-                    </TableCell>
-                    <TableCell sx={{ ...headingStyle }}>Monday</TableCell>
-                    <TableCell sx={{ ...headingStyle }}>Tuesday</TableCell>
-                    <TableCell sx={{ ...headingStyle }}>wednesday</TableCell>
-                    <TableCell sx={{ ...headingStyle }}>thursday</TableCell>
-                    <TableCell sx={{ ...headingStyle }}>friday</TableCell>
-                    <TableCell sx={{ ...headingStyle }}>saturday</TableCell>
-                    <TableCell sx={{ ...headingStyle }}>sunday</TableCell>
+          Our Schedule
+        </Typography>
+
+        {/* Table */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Box sx={{ maxWidth: "1600px", width: "100%" }}>
+            <TableContainer data-aos="fade-left" data-aos-duration="1000">
+              <Table
+                sx={{
+                  "& .MuiTableCell-root": { padding: "10px 5px" },
+                  tableLayout: "auto",
+                  borderCollapse: "separate",
+                  borderSpacing: { xs: "20px 15px", xl: "30px 25px" },
+                }}
+              >
+                {/* Table Header */}
+                <TableRow>
+                  <TableCell
+                    sx={{
+                      ...headingStyle,
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Box sx={{ height: "55px", width: "95px" }}>
+                      <Image
+                        style={{
+                          height: "100%",
+                          width: "100%",
+                          objectFit: "contain",
+                        }}
+                        src={svgs.Clock}
+                        alt="clock"
+                      />
+                    </Box>
+                  </TableCell>
+                  <TableCell sx={{ ...headingStyle }}>Monday</TableCell>
+                  <TableCell sx={{ ...headingStyle }}>Tuesday</TableCell>
+                  <TableCell sx={{ ...headingStyle }}>Wednesday</TableCell>
+                  <TableCell sx={{ ...headingStyle }}>Thursday</TableCell>
+                  <TableCell sx={{ ...headingStyle }}>Friday</TableCell>
+                  <TableCell sx={{ ...headingStyle }}>Saturday</TableCell>
+                  <TableCell sx={{ ...headingStyle }}>Sunday</TableCell>
+                </TableRow>
+
+                {/* Table Rows */}
+                {tableRows.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell sx={{ ...headingStyle }}>{row.time}</TableCell>
+                    {row.activities.map((activity, idx) => (
+                      <TableCell key={idx} sx={{ ...paragraphStyle }}>
+                        <Typography sx={{ ...topic }}>{activity}</Typography>
+                      </TableCell>
+                    ))}
                   </TableRow>
-                  <TableRow>
-                    <TableCell sx={{ ...headingStyle }}>
-                      10:00AM - 11:00AM
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Girls Only</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell sx={{ ...headingStyle }}>
-                      11:00AM - 12:00AM
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>
-                        Afternoon Session
-                      </Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>5-8 Years Old</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>5-8 Years Old</Typography>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell sx={{ ...headingStyle }}>
-                      12:00PM - 1:00PM
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>
-                        Afternoon Session
-                      </Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>
-                        Afternoon Session
-                      </Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>9 Years Old +</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>9 Years Old +</Typography>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell sx={{ ...headingStyle }}>
-                      1:00PM - 2:00PM
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Open Class</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Open Class</Typography>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell sx={{ ...headingStyle }}>
-                      5:30PM - 7:00PM
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Adults & Senior</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Adults & Senior</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Adults & Senior</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Adults & Senior</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Adults & Senior</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell sx={{ ...headingStyle }}>
-                      7:00PM - 8:00PM
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Juniours Boxing</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Juniours Boxing</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Juniours Boxing</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Juniours Boxing</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>Juniours Boxing</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>{" "}
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell sx={{ ...headingStyle }}>
-                      8:00PM - 9:00PM
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>For All</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>For All</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>For All</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>For All</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>For All</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                    <TableCell sx={{ ...paragraphStyle }}>
-                      <Typography sx={{ ...topic }}>-</Typography>
-                    </TableCell>
-                  </TableRow>
-                </Table>
-              </TableContainer>
-            </Box>
+                ))}
+              </Table>
+            </TableContainer>
           </Box>
         </Box>
       </Box>
-    </>
+    </Box>
   );
 }
