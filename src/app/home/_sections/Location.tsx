6b52ebf551f59926829404fda9c1abc5b18489ab
naqash <contact@pulsetechnologysolutions.co.uk>
2025-01-25 17:22:39 +0000

    add map iframe;

--- src/app/home/_sections/Location.tsx
+++ src/app/home/_sections/Location.tsx
@@ -104,7 +104,7 @@ export default function Location() {
                 width="600" 
                 height="450" 
                 style="border:0;" 
-                allowfullscreen="" 
+                // allowfullscreen="" 
                 loading="lazy" 
                 referrerpolicy="no-referrer-when-downgrade">
               </iframe>
