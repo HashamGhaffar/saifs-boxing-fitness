82e7c605b73110df9d78039e264f0e76a7c3502a
naqash <contact@pulsetechnologysolutions.co.uk>
2025-01-25 17:36:05 +0000

    deploy

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -12,33 +12,8 @@ export interface CoachCardProps {
 
 export const Coaches: CoachCardProps[] = [
   {
-    name: "Basit",
-    description: `Coach "Basit" is a seasoned boxing expert with over 15
-years of experience in the ring. Known for his relentless
-drive and attention to detail, he has trained everyone from
-beginners to competitive fighters, tailoring his approach to
-fit each individual’s needs. His unique blend of technical
-mastery and motivational coaching style makes him a favorite
-among those who want to push past their limits and unlock
-their full potential. With a background in both amateur and
-professional boxing, he brings a wealth of knowledge and a
-hands-on approach to every session. His focus is not just on
-perfecting technique, but also on building mental toughness,
-confidence, and discipline qualities.<br><br>
-With a background in both amateur and professional boxing, he
-brings a wealth of knowledge and a hands-on approach to every
-session. His focus is not just on perfecting technique, but
-also on building mental toughness, confidence, and discipline
-qualities.
-`,
-    image: webp.Basit,
-    backgroundImage: "url(bgCoachJames.webp)",
-    navImage: webp.Basit,
-    isSelected: true,
-  },
-  {
-    name: "Daniel",
-    description: `Coach “Daniel Jayson” is a highly respected boxing coach with 
+    name: "Safe",
+    description: `Coach Saif is a highly respected boxing coach with 
     a reputation for transforming fighters into champions. With over a decade of
      experience, his approach combines technical precision, strategic insight, and
       an emphasis on mental resilience. His training sessions are intense, yet supportive, 
@@ -51,6 +26,30 @@ qualities.
     backgroundImage: "url(bgCoachJames.webp)",
     navImage: webp.NavDaniel,
     isSelected: false,
+  },
+  {
+    name: "Basit",
+    description: `Coach Basit is a seasoned boxing expert with over 15
+      years of experience in the ring. Known for his relentless
+      drive and attention to detail, he has trained everyone from
+      beginners to competitive fighters, tailoring his approach to
+      fit each individual’s needs. His unique blend of technical
+      mastery and motivational coaching style makes him a favorite
+      among those who want to push past their limits and unlock
+      their full potential. With a background in both amateur and
+      professional boxing, he brings a wealth of knowledge and a
+      hands-on approach to every session. His focus is not just on
+      perfecting technique, but also on building mental toughness,
+      confidence, and discipline qualities.<br><br>
+      With a background in both amateur and professional boxing, he
+      brings a wealth of knowledge and a hands-on approach to every
+      session. His focus is not just on perfecting technique, but
+      also on building mental toughness, confidence, and discipline
+      qualities.`,
+    image: webp.Basit,
+    backgroundImage: "url(bgCoachJames.webp)",
+    navImage: webp.Basit,
+    isSelected: true,
   },
 ];
 
